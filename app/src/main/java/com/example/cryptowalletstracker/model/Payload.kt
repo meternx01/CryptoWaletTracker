26f77b351a4cfb3ca15e24ccbe0c1c26584cd31a
meter <meter_1@yahoo.com>
2020-08-16 20:47:16 -0500

    fixed build.gradle and Payload.kt

--- app/src/main/java/com/example/cryptowalletstracker/model/Payload.kt
+++ app/src/main/java/com/example/cryptowalletstracker/model/Payload.kt
@@ -1,8 +1,14 @@
 package com.example.cryptowalletstracker.model
 
+import com.squareup.moshi.Json
+
 data class Payload(
+    @field:Json(name = "address")
     var address: String,
+    @field:Json(name = "totalSpent")
     var totalSpent: String,
+    @field:Json(name = "totalReceived")
     var totalReceived: String,
+    @field:Json(name = "balance")
     var balance: String
 )
