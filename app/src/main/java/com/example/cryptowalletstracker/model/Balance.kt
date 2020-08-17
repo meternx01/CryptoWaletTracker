f3802b978b04e3d5acdd547b6f3c94906d6b2af7
meter <meter_1@yahoo.com>
2020-08-16 20:14:21 -0500

    added moshi converter factory and annotations

--- app/src/main/java/com/example/cryptowalletstracker/model/Balance.kt
+++ app/src/main/java/com/example/cryptowalletstracker/model/Balance.kt
@@ -1,9 +1,14 @@
 package com.example.cryptowalletstracker.model
 
+import com.squareup.moshi.Json
+
 
 data class Balance(
+    @field:Json(name = "payload")
     var payload: Payload,
+    @field:Json(name = "coin")
     var coin: String
 )
+
 
 
